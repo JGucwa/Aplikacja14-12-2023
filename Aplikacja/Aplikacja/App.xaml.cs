42df4e32b36f95425fb3614567d7fc9acd03ff04
JGucwa <kubjaxxx@gmail.com>
2023-12-14 19:54:39 +0100

    Dodano

--- Aplikacja/Aplikacja/App.xaml.cs
+++ Aplikacja/Aplikacja/App.xaml.cs
@@ -2,11 +2,25 @@
 using Xamarin.Forms;
 using Aplikacja.Views;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace Aplikacja
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"wydatek.db");
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- Aplikacja/Aplikacja/Views/Szczegoly.xaml.cs
+++ Aplikacja/Aplikacja/Views/Szczegoly.xaml.cs
@@ -12,9 +12,19 @@ namespace Aplikacja.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Szczegoly : ContentPage
     {
-        public Szczegoly()
+        public Szczegoly(DateTime date)
         {
             InitializeComponent();
+
+            List<Wydatek> tmp = new List<Wydatek>();
+            for(int  i = 0; i < App.Database.GetAll().Result.Count;i++)
+            {
+                if(App.Database.GetAll().Result[i].Data == date)
+                {
+                    tmp.Add(App.Database.GetAll().Result[i]);
+                }
+            }
+            Lista.ItemsSource = tmp;
         }
     }
 }
